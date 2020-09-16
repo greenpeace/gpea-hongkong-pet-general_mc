40e95ef414cbc93523d7510a12f79037774fbc35
KFC <hankfchan@gmail.com>
2020-09-16 13:46:58 +0800

    init

---
+++ build/precache-manifest.7c72ddbaf230cf29452b65c175b940b5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7bdde9dc82507f2d8a05f2829a18f59",
+    "url": "https://api.greenpeace.org.hk/2020/petition/zh-hk.2020.forests.amazon_forest.general.signup.na.mc/index.html"
+  },
+  {
+    "revision": "05a8748f693ae211c3b5",
+    "url": "https://api.greenpeace.org.hk/2020/petition/zh-hk.2020.forests.amazon_forest.general.signup.na.mc/static/css/main.ea0949c3.chunk.css"
+  },
+  {
+    "revision": "05a8748f693ae211c3b5",
+    "url": "https://api.greenpeace.org.hk/2020/petition/zh-hk.2020.forests.amazon_forest.general.signup.na.mc/static/js/main.23ff0150.chunk.js"
+  },
+  {
+    "revision": "b650676da4ea77977b8c",
+    "url": "https://api.greenpeace.org.hk/2020/petition/zh-hk.2020.forests.amazon_forest.general.signup.na.mc/static/js/runtime-main.5cb5ec09.js"
+  }
+]);
